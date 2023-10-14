939d7842812c0ca69643ba2c829364cc0aa52805
Obua Emmanuel <eobua6882@gmail.com>
2023-10-14 14:27:22 +0300

    updated the views.py with ModelViewSet that provides default views for CRUD operations on a Django model

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Student, Teacher
+from .serializers import StudentSerializer, TeacherSerializer
 
-# Create your views here.
+# ModelViewSet provides default views for CRUD operations on a Django model
+
+# We are setting queryset attribute to retrieve all instances of the Student and Teacher models from the database. 
+# The viewsets will handle CRUD operations for these models. 
+
+# ----------------------------------------------------------------------------------------------------------------
+
+# The serializer classes for the models defined below is for serializing and deserializing data when handling API requests.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
