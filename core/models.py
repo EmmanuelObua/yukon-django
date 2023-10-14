68da1b548993228c9a28cd9aabb7ad058219347b
Obua Emmanuel <eobua6882@gmail.com>
2023-10-14 14:00:04 +0300

    defined the Student and Teacher models with their respective fields in the models.py file,

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Student model with name and surname, returning a name and surname cocatinated string
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+# Teacher model with name and students with a many to many relationship returning a name string
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student, related_name='teachers')
+
+    def __str__(self):
+        return self.name
