f641fc00d003c0c8d3f710e8302207f2bf00c4bd
Obua Emmanuel <eobua6882@gmail.com>
2023-10-15 10:21:41 +0300

    creating teachers now includes storing students names

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Student, Teacher
 from .serializers import StudentSerializer, TeacherSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # ModelViewSet provides default views for CRUD operations on a Django model
 
@@ -18,3 +21,20 @@ class StudentViewSet(viewsets.ModelViewSet):
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Extract the student IDs from the request data
+        student_ids = request.data.get('students', [])
+
+        # Create a new teacher instance
+        teacher_serializer = self.get_serializer(data=request.data)
+        if teacher_serializer.is_valid():
+            teacher = teacher_serializer.save()
+
+            # Set the many-to-many relationship with students
+            teacher.students.set(student_ids)
+            teacher.save()
+
+            return Response({"message": "Teacher created successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "There was a slight error, please try again"}, status=status.HTTP_400_BAD_REQUEST)
--- student_teacher_app/settings.py
+++ student_teacher_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'student_teacher_app.urls'
@@ -122,3 +124,26 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Allow requests from any origin (not recommended for production)
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
+
+# Specify the allowed origins (replace this with your frontend's URL)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS = [
+    "Accept",
+    "Content-Type",
+]
